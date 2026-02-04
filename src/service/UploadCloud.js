07d11811f375a54ed711f81bf71d7f701402e39c
thapaamit03 <amitthapamagar8@gmail.com>
2026-02-04 19:27:29 +0545

    add cloudinary configuration and update upload service for image handling

--- src/service/UploadCloud.js
+++ src/service/UploadCloud.js
@@ -1,10 +1,33 @@
-
+const { error, Logger } = require('winston')
+const cloudinary=require('../config/cloudinaryConfig')
 const uploadToCloudinary=async(file)=>{
 
+    return new Promise((resolve,reject)=>{
+        const uploadStream=cloudinary.uploader.upload_stream(
+            {
+                resource_type:'auto'
+            },(error,result)=>{
+                if(error){
+                    console.error('error while uploading',error)
+                    reject(error)
+                }else{
+                    resolve(result)
+                }
+            }
+        )
+        uploadStream.end(file.buffer);
+    })
 
 }
 
-const deleteFromCloudinary=async()=>{
-
+const deleteFromCloudinary=async(publicId)=>{
+    try {
+        const deletedImage=await cloudinary.uploader.destroy(publicId,{
+            resource_type:'auto'
+        })
+        return deletedImage;
+    } catch (error) {
+        console.error("error wile deleting image from cloudinary",error);
+    }
 }
 module.exports={uploadToCloudinary,deleteFromCloudinary}
